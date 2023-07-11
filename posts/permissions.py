af04537e1579ccd50de08839e356a52c0609e91c
rgranatodutra <r.granatodutra@gmail.com>
2023-07-11 03:19:26 -0300

    chore: remove prints from permission

--- posts/permissions.py
+++ posts/permissions.py
@@ -10,16 +10,34 @@ class IsUserIncludedInPostPrivacy(BasePermission):
 
         if user == obj.user:
             post_user_friends = post_user.get_friends()
-            print(post_user_friends)
             return True
 
         if obj.share_privacy == "public":
             return True
         elif obj.share_privacy == "only_friends":
             post_user_friends = post_user.get_friends()
-            print(post_user_friends)
             return user in post_user_friends
         elif obj.share_privacy == "only_me":
             return user.pk == obj.user
         else:
             return user.pk == obj.user
+
+
+class IsUserIncludedInCommentsPostPrivacy(BasePermission):
+    def has_object_permission(self, request, view, obj: Post):
+        user: User = request.user
+        post_user: User = obj.user
+
+        if user == obj.user:
+            post_user_friends = post_user.get_friends()
+            return True
+
+        if obj.comments_privacy == "public":
+            return True
+        elif obj.comments_privacy == "only_friends":
+            post_user_friends = post_user.get_friends()
+            return user in post_user_friends
+        elif obj.comments_privacy == "only_me":
+            return user == obj.user
+        else:
+            return user == obj.user
