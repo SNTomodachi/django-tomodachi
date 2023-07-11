d4cde1366bac85ab5e35c60a11752aaeeb5b78f9
natabgaf <natanfernandes52@gmail.com>
2023-07-11 15:52:02 -0300

    feat/get friendship pending

--- relationships/urls.py
+++ relationships/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RelationshipsView, RelationshipsUpdateView,FollowersView
+from .views import RelationshipsView, RelationshipsUpdateView,FollowersView ,FriendshipView
 
 urlpatterns = [
     path("users/relationships/<int:pk>/", RelationshipsView.as_view()),
+    path("users/friendship", FriendshipView.as_view()),
     path("users/friendship/<int:pk>/", RelationshipsUpdateView.as_view()),
     path("users/following/<int:pk>/", FollowersView.as_view()),
     
